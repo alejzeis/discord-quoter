833b8be0bc0519235869a89fc4ab3049fc62bf4d
jython234 <atzeise@gmail.com>
2019-05-27 15:03:41 -0500

    Rewrite to use MongoDB, support for unquoting via reaction removal

---
+++ db.js
@@ -0,0 +1,133 @@
+var nextId;
+
+function randomInt(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function loadNextId(db) {
+    let collection = db.collection("metadata");
+    return new Promise((resolve, reject) => {
+        // Load nextId value
+        collection.find({ key:"nextId" }).toArray((err, docs) => {
+            if(err) {
+                reject(err);
+            } else {
+                if(docs[0]) {
+                    nextId = docs[0].value;
+                    resolve();
+                } else {
+                    collection.insertOne({
+                        key: "nextId",
+                        value: 0
+                    }, (err, result) => {
+                        if(err) reject(err);
+                        else {
+                            nextId = 0;
+                            resolve();
+                        }
+                    });
+                }
+            }
+        });
+    });
+}
+
+function updateNextId(collection) {
+    nextId = nextId + 1;
+
+    return new Promise((resolve, reject) => {
+        collection.updateOne({
+            key: "nextId"
+        }, { $set: { value: nextId}}, (err, result) => {
+            if(err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+function insertPinnedMessage(db, snowflake, senderId, sender, content) {
+    return new Promise((resolve, reject) => {
+        let collection = db.collection("quotes");
+        let metaCollection = db.collection("metadata");
+        let quoteId = nextId;
+
+        collection.insertOne({
+            snowflake: snowflake,
+            quoteId: quoteId,
+            senderId: senderId,
+            sender: sender,
+            content: content
+        }, (err, result) => {
+            if(!err) {
+                updateNextId(metaCollection).then(() => resolve(quoteId)).catch((err) => reject(err));
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
+
+function removePinnedMessage(db, quoteId) {
+    return new Promise((resolve, reject) => {
+        let collection = db.collection("quotes");
+
+        collection.deleteOne({
+            quoteId: quoteId
+        }, (err, result) => {
+            if(err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+function removePinnedMessageBySnowflake(db, snowflake) {
+    return new Promise((resolve, reject) => {
+        let collection = db.collection("quotes");
+
+        collection.deleteOne({
+            snowflake: snowflake
+        }, (err, result) => {
+            if(err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+function findQuoteById(db, quoteId) {
+    return new Promise((resolve, reject) => {
+       let collection = db.collection("quotes");
+
+       collection.findOne({
+           quoteId: quoteId
+       }, (err, result) => {
+          if(err) reject(err);
+          else resolve(result);
+       });
+    });
+}
+
+function findRandomQuoteAny(db) {
+    return new Promise((resolve, reject) => {
+       let collection = db.collection("quotes");
+
+       collection.find({}).toArray((err, docs) => {
+          if(err) reject(err);
+          else if(docs.length < 1) resolve(null);
+          else {
+              // Not subtracting 1 from array length since the max integer parameter is exclusive
+              let index = randomInt(0, docs.length);
+              resolve(docs[index]);
+          }
+       });
+    });
+}
+
+module.exports = {
+    loadNextId: loadNextId,
+    insertPinnedMessage: insertPinnedMessage,
+    removePinnedMessage: removePinnedMessage,
+    removePinnedMessageBySnowflake: removePinnedMessageBySnowflake,
+    findQuoteById: findQuoteById,
+    findRandomQuoteAny: findRandomQuoteAny
+};
+
---
+++ events.js
@@ -0,0 +1,111 @@
+let dbhelp = require("./db.js");
+
+let os = require("os");
+
+const software = "discord-quoter";
+const version = "2.1.0";
+const author = "jython234";
+
+function onReactionAdded(config, db, messageReaction, user) {
+    if(messageReaction.emoji.name === config.reactionName) {
+        // Quote this message
+
+        // Don't allow quoting ourself
+        if(messageReaction.message.member.user.username === config.username) return;
+
+        let name = "\"" + messageReaction.message.member.displayName + "\"";
+        dbhelp.insertPinnedMessage(db, messageReaction.message.id, messageReaction.message.author.id, messageReaction.message.member.displayName, messageReaction.message.cleanContent).then((msgId) => {
+            messageReaction.message.channel.send("<@" + user.id + ">, message quoted with ID " + msgId);
+        }).catch((err) => {
+            messageReaction.message.channel.send("<@" + user.id + ">, failed to quote message: Database I/O error");
+            console.error("Error while saving a quoted a message!");
+            console.error(err);
+        });
+    }
+}
+
+function onReactionRemoved(config, db, messageReaction, user) {
+    if(messageReaction.emoji.name === config.reactionName) {
+        // try to un-Quote this message
+
+        dbhelp.removePinnedMessageBySnowflake(db, messageReaction.message.id).then(() => {
+            messageReaction.message.channel.send("<@" + user.id + ">, deleted quote.");
+        }).catch((err) => {
+            console.error("Error while trying to delete quoted message via reaction removal");
+            console.error(err);
+        });
+    }
+}
+
+function onMessage(db, msg) {
+    if (msg.content.startsWith("!")) {
+        if(msg.content.startsWith("!info")) {
+            msg.reply("I'm running " + software + " v" + version + " by " + author + " on " + os.platform() + " at " + os.hostname());
+        } else if(msg.content.startsWith("!addquote")) {
+            let exploded = msg.cleanContent.split(" ");
+
+            if(exploded.length < 2) {
+                msg.reply("Usage: !addquote [SNOWFLAKE]");
+            } else {
+                let message = msg.channel.messages.get(exploded[1]);
+                if (!message)
+                    msg.reply("Invalid Snowflake");
+                else {
+                    dbhelp.insertPinnedMessage(db, message.id, message.author.id, message.member.displayName, message.cleanContent).then((quoteId) => {
+                        msg.reply("message quoted with ID " + value);
+                    }).catch((err) => {
+                        msg.reply("failed to quote message: Database I/O error");
+                        console.error("Error while processing !addquote command:");
+                        console.error(err);
+                    });
+                }
+            }
+        } else if(msg.content.startsWith("!randquote")) {
+            dbhelp.findRandomQuoteAny(db).then((doc) => {
+                if(doc == null) {
+                    msg.channel.send("**Database is empty**");
+                } else {
+                    msg.channel.send("**[ID: " + doc.quoteId + "]** " + doc.sender + ": " + doc.content);
+                }
+            }).catch((err) => {
+                msg.reply("failed to find a message: Database I/O error");
+                console.error("Error while processing !randquote command:");
+                console.error(err);
+            });
+        } else if(msg.content.startsWith("!delquote")) {
+            let exploded = msg.cleanContent.split(" ");
+
+            if(exploded.length < 2) {
+                msg.reply("Usage: !delquote [ID]");
+            } else {
+                dbhelp.removePinnedMessage(db, parseInt(exploded[1])).then(() => {
+                    msg.reply("Quote " + exploded[1] + " deleted.");
+                }).catch((err) => {
+                    msg.reply("I couldn't find that quote");
+                    console.error("Error while trying to find quote to delete: ");
+                    console.error(err);
+                });
+            }
+        } else if(msg.content.startsWith("!getquote")) {
+            let exploded = msg.cleanContent.split(" ");
+
+            if(exploded.length < 2) {
+                msg.reply("Usage: !getquote [ID]");
+            } else {
+                dbhelp.findQuoteById(db, parseInt(exploded[1])).then((doc) => {
+                    msg.channel.send("**[ID: " + exploded[1] + "]** " + doc.sender + ": " + doc.content);
+                }).catch((err) => {
+                    msg.reply("I couldn't find that quote");
+                    //console.error("Error while processing !getquote command:");
+                    //console.error(err);
+                });
+            }
+        }
+    }
+}
+
+module.exports = {
+    onReactionAdded: onReactionAdded,
+    onReactionRemoved: onReactionRemoved,
+    onMessage: onMessage
+};
